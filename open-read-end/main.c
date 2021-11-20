2422d4f3c0427df9ec3dd48838ae57762a233637
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-20 17:49:56 +0900

    header

--- open-read-end/main.c
+++ open-read-end/main.c
@@ -1,8 +1,13 @@
 #include <fcntl.h>
+#include <unistd.h>
 
 int	main()
 {
 	int fd = open("text.txt", O_RDONLY);
 	// int fd = creat(, O_RDONLY);
+	// int fd = open("../text.txt", O_RDONLY);
+	char* str;
+	int red_num = read(fd ,str , 10);
+	printf("%s\n%d", str, red_num);
 	return 0;
 }
