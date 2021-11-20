00f93dccc0ddb01482c8b74bb45441c4bd4099ca
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-20 18:18:52 +0900

    realtive path

--- open-read-end/main.c
+++ open-read-end/main.c
@@ -4,11 +4,11 @@
 
 int	main()
 {
-	int fd = open("text.txt", O_RDONLY);
+//	int fd = open("text.txt", O_RDONLY);
 	// int fd = creat(, O_RDONLY);
-	// int fd = open("../text.txt", O_RDONLY);
+	int fd = open("../text.txt", O_RDONLY);
 	char* str;
-	int red_num = read(fd ,str , 10);
+	size_t red_num = read(fd ,str , 10);
 	printf("%s\n%d", str, red_num);
 	close(fd);
 	return 0;
