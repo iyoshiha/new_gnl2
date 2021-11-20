77e37ea10911ab08c8cec7ee36f0b94001cca791
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-21 08:30:24 +0900

    checking behavior of read()

--- mem_leak/main.c
+++ mem_leak/main.c
@@ -2,34 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "../../ft_printf/libft/libft.h"
 
-int	r(int fd, char* str)
+int	r(int fd, char* str, int i)
 {
-	static int i;
-
 	printf("this is %d times\n", i);
-	i++;
 	return (read(fd ,str , 100));
 }
 
 int	main()
 {
-int fd = open("e_text", O_RDONLY);
+int fd = open("text", O_RDONLY);
 char* str;
-// char fakep;
+int i = 0;
 size_t red_num;
+// char fakep;
 
 str = malloc(sizeof(char)*128);
 // str = calloc(sizeof(char),128);
 while (i < 3)
 {
-	r(fd, str);
-	printf("%s \n", str);
+	red_num = r(fd, str, i);
+	i++;
+	puts("#############");
+	ft_putstr_fd(str, STDOUT_FILENO);
+	puts("#############");
+	printf("\n%zu\n", red_num);
 }
 // printf("%zu\n", red_num);
 // printf("%s\n", str);
 // str = &fakep;
 // free(str);
-// close(fd);
+close(fd);
 return 0;
 }
