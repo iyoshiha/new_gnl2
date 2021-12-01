8fc174dda050fc5f9634a7f80d2bc43e5aab3bb3
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-12-02 02:54:55 +0900

    defed cond exp for err and ret all read all line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:32 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/01 18:23:10 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/02 02:51:57 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,27 @@
 #define BUFFER_SIZE
 #endif
 
-typedef struct s_data
+typedef struct s_txt
 {
-	char	*save;
 	char	buf[BUFFER_SIZE + 1];
-	char	*tmp;
-	ssize_t	suxes_read_num;
-	char	*line;
-
-}				t_data;
+	ssize_t	len_read;
+}				t_txt;
 
 char	*get_next_line(int fd)
 {
-	char	*save
-	read(fd, buf, BUFFER_SIZE);
+	static char	*save;
+	char		*line;
+	t_txt		txt;
 
+	txt.len_read = 0;
+	// exam save
+	txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
+	if (txt.len_read < 0 || txt.len_read && save == NULL) // if error  // if EOF && save == NULL
+		return (NULL);
+	if (txt.len_read) // required info is if txt was read or not here. save doesnt matter coz above conditional expression exclude read val
+	{
+		save_buf();
+		str
+	}
 	return line;
 }
