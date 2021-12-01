d89349d21934cec9a221941c8e2e6cfd010e95fc
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-12-01 18:23:30 +0900

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,29 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:32 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/20 17:49:46 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/01 18:23:10 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#ifndef BUFFER_SIZE // delete here when submit
+#define BUFFER_SIZE
+#endif
+
+typedef struct s_data
+{
+	char	*save;
+	char	buf[BUFFER_SIZE + 1];
+	char	*tmp;
+	ssize_t	suxes_read_num;
+	char	*line;
+
+}				t_data;
+
+char	*get_next_line(int fd)
+{
+	char	*save
+	read(fd, buf, BUFFER_SIZE);
+
+	return line;
+}
