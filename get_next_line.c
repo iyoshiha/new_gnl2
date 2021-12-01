5c75e805e1f415d63209414d036689273d2f830a
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-12-02 03:30:30 +0900

    described by natural lang

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:32 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/02 02:59:47 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/02 03:30:07 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ typedef struct s_txt
 	ssize_t	len_read;
 }				t_txt;
 
+save_buf(t_txt txt)
+{
+
+	return;
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*save;
@@ -29,14 +35,27 @@ char	*get_next_line(int fd)
 
 	txt.len_read = 0;
 	// exam save
-	txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
-	if (txt.len_read < 0 || txt.len_read && save == NULL) // if error  // if EOF && save == NULL
-		return (NULL);
-	if (txt.len_read) // required info is if txt was read or not here. save doesnt matter coz above conditional expression exclude read val
-	{ // len is only positive(successfully read) or 0 (all txt was read)
-		buf[BUFFER_SIZE + 1] == '\0';
-		save_buf();
-		str
+	while ()
+	{
+		txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
+		if (txt.len_read < 0 || txt.len_read && save == NULL) // if error  // if EOF && save == NULL
+			return (NULL);
+		if (txt.len_read == END_OF_FILE)
+			jump if search_break() ture
+			end_make(line);
+			if search was flase
+			{
+				make(line)
+			}
+		if (txt.len_read) // required info is if txt was read or not here. save doesnt matter coz above conditional expression exclude read val
+		{				  // len is only positive(successfully read) or 0 (all txt was read)
+			txt.buf[BUFFER_SIZE + 1] == '\0';
+			save_buf(txt);
+		}
+		if (search_break() is TRUE)
+			break;
 	}
+	make(line);
+
 	return line;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:07 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/02 03:03:57 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/02 03:26:20 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #ifdef BUF
 #define BUFFER_SIZE 5
 #endif
-
+#define END_OF_FILE 0
 
 
 // ssize_t range is [-1 =< ssize_t =<  SSIZE_MAX]
