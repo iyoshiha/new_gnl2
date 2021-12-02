359d2bc5e4f87f34880dbbd5c0a9c1aaa88d0d7d
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-12-03 02:34:35 +0900

    find_break_line func

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:32 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/03 00:38:02 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/03 02:34:22 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@
 typedef struct s_txt
 {
 	char	buf[BUFFER_SIZE + 1];
-	ssize_t	len_read;
-}				t_txt;
+	int		flag;
+	char	*line;
+	ssize_t len_read;
+} t_txt;
 
 save_buf(t_txt txt)
 {
@@ -28,35 +30,45 @@ save_buf(t_txt txt)
 	return;
 }
 
-int		find_break_line();
-void	save_buf();
-
-
-char	*get_next_line(int fd)
+int find_break_line(char *save);
 {
-	static char	*save;
-	char		*line;
-	t_txt		txt;
+	int	i;
+
+	i = 0;
+	if (save == NULL) // why this save is not recognized???
+		return (GNL_NOT_FOUND);
+	while (save[i] != '\0')
+	{
+		if (save[i] == '\n')
+			return (GNL_FOUND);
+	}
+	return (GNL_NOT_FOUND);
+}
+void save_buf(char *save, t_txt *txt);
+void move_save_to_line(char *line, char *save, int flag);
+
+char *get_next_line(int fd)
+{
+	static char *save;
+	t_txt txt;
 
 	txt.len_read = 0;
-	if (find_break_line(save) == GNL_NOT_FOUND_BREAK)
-	{
-		while (REACH_EOF_OR_FOUND_BREAK)
+	txt.flag = GNL_UNTIL_BREAK;
+	if (find_break_line(save) == GNL_NOT_FOUND)
+		while (UNTIL_REACH_EOF_OR_FOUND_BREAK)
 		{
 			txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
 			if (txt.len_read < 0 || txt.len_read && save == NULL)
 				return (NULL);
-				// ^1
 			if (txt.len_read == END_OF_FILE)
-				break; // need to go to ^2
-			save_buf(): // when reach here without break means read_num is positive!
-			if (find_break_line() == GNL_FOUND_BREAK)
+				break;
+			save_buf(save, &txt);
+			if (find_break_line(save) == GNL_FOUND)
 				break;
 		}
-		if (txt.len_read == END_OF_FILE)
-			if (find_break_line() == GNL_NOT_FOUND_BREAK)
-				copy_save_to_line(, GNL_ALL);
-				return (line);
-		copy_save_to_line(, GNL_UNTIL_BREAK);
-		return (line);
+	if (txt.len_read == END_OF_FILE)
+		if (find_break_line() == GNL_NOT_FOUND)
+			txt.flag = GNL_ALL;
+	move_save_to_line(&txt.line, save, txt.flag);
+	return (txt.line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:07 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/03 00:21:15 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/03 02:27:00 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,16 @@
 #endif
 #define END_OF_FILE 0
 
-#define GNL_FOUND_BREAK 1
-#define GNL_NOT_FOUND_BREAK 0
-#define REACH_EOF_OR_FOUND_BREAK 1
+#define GNL_FOUND 1
+#define GNL_NOT_FOUND 0
+#define UNTIL_REACH_EOF_OR_FOUND_BREAK 1
 #define GNL_ALL 1
 #define GNL_UNTIL_BREAK 0
 // ssize_t range is [-1 =< ssize_t =<  SSIZE_MAX]
 #endif
+
+
+
+int find_break_line(char *save);
+void save_buf(char *save, t_txt *txt);
+void move_save_to_line(char *line, char *save, int flag);
