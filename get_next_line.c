e48c02be36080b6378ffbf0b79c3c43c9d45d547
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-12-03 00:36:54 +0900

    done construction

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:32 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/02 23:36:36 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/03 00:36:50 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,35 +38,24 @@ char	*get_next_line(int fd)
 	t_txt		txt;
 
 	txt.len_read = 0;
-	// exam save
-	while ()
+	if (find_break_line(save) == GNL_NOT_FOUND_BREAK)
 	{
-		txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
-						if (txt.len_read < 0 || txt.len_read && save == NULL) // if error  // if EOF && save == NULL
-							return (NULL);
-			/////////////////////////
-		if (txt.len_read == END_OF_FILE) // ^1
-			// true means ^2 |
-			jump if find_breakline() ture
-			end_make(line);
-			/////////////////////////
-			if search was flase // same excution: code 1
-			{
-				make(line)
-			}
-		if (txt.len_read) // required info is if txt was read or not here. save doesnt matter coz above conditional expression exclude read val
-		{				  // len is only positive(successfully read) or 0 (all txt was read)
-			// ^ 3
-			txt.buf[BUFFER_SIZE + 1] == '\0';
-			save_buf(txt);
-					if (find_breakline() is TRUE)// ^3 | same excution: code 1
-							break;
-		}
-	}
-	if find_breakline(save) == GNL_FOUND // ^5
-		save -> line ;
-		free(save);
-		save = NULL pointer;
-	make(line); // ^4
-	return line;
+		while (REACH_EOF_OR_FOUND_BREAK)
+		{ /// loooop
+			txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
+			if (txt.len_read < 0 || txt.len_read && save == NULL)
+				return (NULL);
+				// ^1
+			if (txt.len_read == END_OF_FILE)
+				break; // need to go to ^2
+			save_buf(): // when reach here without break means read_num is positive!
+			if (find_break_line() == GNL_FOUND_BREAK)
+				break;
+		} //// loooooooop end
+		if (txt.len_read == END_OF_FILE)
+			if (find_break_line() == GNL_NOT_FOUND_BREAK)
+				copy_save_to_line(, GNL_ALL);
+				return (line);
+		copy_save_to_line(, GNL_UNTIL_BREAK);
+		return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:07 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/02 23:38:37 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/03 00:21:15 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@
 
 #define GNL_FOUND_BREAK 1
 #define GNL_NOT_FOUND_BREAK 0
+#define REACH_EOF_OR_FOUND_BREAK 1
+#define GNL_ALL 1
+#define GNL_UNTIL_BREAK 0
 // ssize_t range is [-1 =< ssize_t =<  SSIZE_MAX]
 #endif
