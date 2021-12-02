06f8d71a8fce01043304d1d11a1303a916050e63
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-12-02 23:38:45 +0900

    macros

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:32 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/02 03:31:35 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/02 23:36:36 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "get_next_line.h"
 #ifndef BUFFER_SIZE // delete here when submit
 #define BUFFER_SIZE
 #endif
@@ -27,6 +28,9 @@ save_buf(t_txt txt)
 	return;
 }
 
+int	find_break_line();
+
+
 char	*get_next_line(int fd)
 {
 	static char	*save;
@@ -38,24 +42,31 @@ char	*get_next_line(int fd)
 	while ()
 	{
 		txt.len_read = read(fd, txt.buf, BUFFER_SIZE);
-		if (txt.len_read < 0 || txt.len_read && save == NULL) // if error  // if EOF && save == NULL
-			return (NULL);
-		if (txt.len_read == END_OF_FILE)
-			jump if search_break() ture
+						if (txt.len_read < 0 || txt.len_read && save == NULL) // if error  // if EOF && save == NULL
+							return (NULL);
+			/////////////////////////
+		if (txt.len_read == END_OF_FILE) // ^1
+			// true means ^2 |
+			jump if find_breakline() ture
 			end_make(line);
+			/////////////////////////
 			if search was flase // same excution: code 1
 			{
 				make(line)
 			}
 		if (txt.len_read) // required info is if txt was read or not here. save doesnt matter coz above conditional expression exclude read val
 		{				  // len is only positive(successfully read) or 0 (all txt was read)
+			// ^ 3
 			txt.buf[BUFFER_SIZE + 1] == '\0';
 			save_buf(txt);
+					if (find_breakline() is TRUE)// ^3 | same excution: code 1
+							break;
 		}
-		if (search_break() is TRUE)// same excution: code 1
-			break;
 	}
-	make(line);
-
+	if find_breakline(save) == GNL_FOUND // ^5
+		save -> line ;
+		free(save);
+		save = NULL pointer;
+	make(line); // ^4
 	return line;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,19 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 17:46:07 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/12/02 03:26:20 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/12/02 23:38:37 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 #define BUF
 #ifdef BUF
 #define BUFFER_SIZE 5
 #endif
 #define END_OF_FILE 0
 
-
+#define GNL_FOUND_BREAK 1
+#define GNL_NOT_FOUND_BREAK 0
 // ssize_t range is [-1 =< ssize_t =<  SSIZE_MAX]
+#endif
